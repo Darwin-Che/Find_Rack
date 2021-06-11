4788747f772853444a46e6a6a00003566fbbf110
Zhaocheng Che <chezhaocheng@outlook.com>
2021-06-11 14:09:41 -0400

    settle table definition

--- test/create_table.sql
+++ test/create_table.sql
@@ -1,36 +1,41 @@
 CREATE TABLE Movies (
     titleid     VARCHAR(11)  NOT NULL PRIMARY KEY,
-    title       VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    releasedate    DATE,
-    region      VARCHAR(30),
-    lang        VARCHAR(30)
+    title       VARCHAR(100) NOT NULL,
+    releasedate    DATE
 );
 
 CREATE TABLE Users (
     userid     VARCHAR(11)  NOT NULL PRIMARY KEY,
-    username   VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL 
+    username   VARCHAR(20)  NOT NULL 
 );
 
--- CREATE TABLE Casts (
---     castid      VARCHAR(11) NOT NULL PRIMARY KEY,
---     titleid     VARCHAR(11) NOT NULL 
---     castname    VARCHAR(50) NOT NULL,
---     FOREIGN KEY (titleid) REFERENCES Movies(titleid)
--- );
+CREATE TABLE Casts (
+    castid      VARCHAR(11) NOT NULL PRIMARY KEY,
+    castname    VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE Cast_Movie (
+    castid      VARCHAR(11)  NOT NULL,
+    titleid     VARCHAR(11)  NOT NULL,
+    roll        VARCHAR(20)  NOT NULL,
+    PRIMARY KEY (castid, titleid, roll),
+    FOREIGN KEY (castid) REFERENCES Casts(castid),
+    FOREIGN KEY (titleid) REFERENCES Movies(titleid)
+);
 
 CREATE TABLE Lists (
     listid      VARCHAR(11) NOT NULL PRIMARY KEY,
     userid      VARCHAR(11) NOT NULL,
-    listname    VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    listname    VARCHAR(20) NOT NULL,
     FOREIGN KEY (userid) REFERENCES Users(userid)
 );
 
 CREATE TABLE Comments (
+    commentid   VARCHAR(11) NOT NULL PRIMARY KEY,
     titleid     VARCHAR(11) NOT NULL,
     userid      VARCHAR(11) NOT NULL,
-    comment     LONGTEXT    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    comment     LONGTEXT    NOT NULL,
     publishtime DATETIME    NOT NULL,
-    PRIMARY KEY (titleid, userid, publishtime),
     FOREIGN KEY (userid) REFERENCES Users(userid),
     FOREIGN KEY (titleid) REFERENCES Movies(titleid)
 );
