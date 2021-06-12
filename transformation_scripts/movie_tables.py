2d908b54bd0db9053ee779a01d02c22b768e9b3e
n-ivan <n-ivan@users.noreply.github.com>
2021-06-12 12:34:04 -0400

    typo fix

--- transformation_scripts/movie_tables.py
+++ transformation_scripts/movie_tables.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-title_basics = pd.read_csv('title.crew.tsv', sep='\t').drop(columns=['titleType', 'primaryTitle', 'isAdult', 'endYear'])
+title_basics = pd.read_csv('title.basics.tsv', sep='\t').drop(columns=['titleType', 'primaryTitle', 'isAdult', 'endYear'])
 genre_dict = {'tconst': [], 'genre': []}
 
 for index, row in title_basics.iterrows():
@@ -10,7 +10,7 @@ for index, row in title_basics.iterrows():
             genre_dict['genre'].append(genre)
 
 movie_genre = pd.DataFrame(data=genre_dict)
-title_basics.drop(columns=['genres'])
+title_basics = title_basics.drop(columns=['genres']).rename(columns={'startYear': 'release_year', 'originalTitle': 'title'})
 
 title_basics.to_csv('movies.scsv', sep=';', index=False)
 movie_genre.to_csv('movie_genre.scsv', sep=';', index=False)
