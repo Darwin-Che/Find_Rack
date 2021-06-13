efa2e71362e15172519e93c2c8a5e24a0639c22a
nulldev <contact@andybao.me>
2021-06-13 17:53:19 -0400

    Add ability to query for users + code cleanup

--- html/script.js
+++ html/script.js
@@ -1,21 +1,51 @@
-async function query_movie(element) {
-    const titleInput = document.getElementById("query_movie_title");
+async function get(endpoint, params) {
+    const result = await fetch(endpoint + '?' + new URLSearchParams(params).toString());
+    if(result.ok) {
+        return await result.json();
+    } else {
+        throw new Error('Bad HTTP status code!');
+    }
+}
+
+async function post(endpoint, body) {
+    const result = await fetch(endpoint, {
+        method: 'POST', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(body)
+    });
+
+    return await result.json();
+}
+
+function setResult(element, result) {
+    element.parentElement.querySelector(".out").textContent = result;
+}
+
+async function query_movies(element) {
+    const titleInput = document.getElementById("query_movies_title");
     const params = {};
     if(titleInput.value.length > 0) {
         params.title = titleInput.value;
     }
     let outText;
     try {
-        const result = await fetch('/api/movies?' + new URLSearchParams(params).toString());
-        if(result.ok) {
-            outText = "Movies:\n" + (await result.json()).map(it => it[1]).join('\n')
-        } else {
-            throw new Error('Bad HTTP status code!');
-        }
+        const result = await get('/api/movies', params);
+        outText = "Movies:\n" + result.map(it => it[1]).join('\n');
     } catch(e) {
         outText = "Error: " + e.message;
     }
-    element.parentElement.querySelector(".out").textContent = outText;
+    setResult(element, outText);
+}
+
+async function query_users(element) {
+    let outText;
+    try {
+        const result = await get('/api/users', {});
+        outText = "Users:\n" + result.map(it => `userid: ${it[0]}, username: ${it[1]}`).join('\n');
+    } catch(e) {
+        outText = "Error: " + e.message;
+    }
+    setResult(element, outText);
 }
 
 async function create_user(element) {
@@ -23,20 +53,14 @@ async function create_user(element) {
     let outText;
     try {
         if(usernameInput.value.length <= 0) throw new Error('Missing username!');
-        const result = await fetch('/api/users', {
-            method: 'POST', 
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify({
-                username: usernameInput.value
-            }
-        )});
+        const result = await post('/api/users', {username: usernameInput.value});
         if(result.ok) {
-            outText = "User created, userid is: " + (await result.json()).userid;
+            outText = "User created, userid is: " + result.userid;
         } else {
             throw new Error('Bad HTTP status code!');
         }
     } catch(e) {
         outText = "Error: " + e.message;
     }
-    element.parentElement.querySelector(".out").textContent = outText;
+    setResult(element, outText);
 }
