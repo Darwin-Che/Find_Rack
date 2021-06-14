2617b447c317a255d6c25e684ad3f8dd6e70c374
Yao Wang <y2833wang@uwaterloo.ca>
2021-06-14 00:43:16 -0400

    update

--- pyflask/test-sample.py
+++ pyflask/test-sample.py
@@ -22,10 +22,10 @@ cursor.execute(query1, (input_title,))
 for r in cursor.fetchall():
     print(r)
 
-print('Searching by actor')
+print('Searching by actor/actress')
 
-input_actor = ''
-input_title = format_input(input_actor)
+input_actor = 'Fox'
+input_actor = format_input(input_actor)
 
 query2 = '''
 SELECT title,releaseyear,runtimemin FROM Movies
@@ -34,6 +34,21 @@ WHERE titleid IN
 WHERE castname LIKE %s AND (role="actor" OR role="actress"))
 '''
 cursor.execute(query2, (input_actor,))
+for r in cursor.fetchall():
+    print(r)
+
+print('Searching by director')
+
+input_director = 'James Cameron'
+input_director = format_input(input_director)
+
+query3 = '''
+SELECT title,releaseyear,runtimemin FROM Movies
+WHERE titleid IN
+(SELECT titleid FROM Casts c JOIN Cast_Movie cm ON c.castid=cm.castid
+WHERE castname LIKE %s AND role="director")
+'''
+cursor.execute(query3, (input_director,))
 for r in cursor.fetchall():
     print(r)
 
