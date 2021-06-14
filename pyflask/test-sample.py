f42442b59c7c5b8c225f4a6fd269325863825c7a
Yao Wang <y2833wang@uwaterloo.ca>
2021-06-13 23:45:10 -0400

    update

--- pyflask/test-sample.py
+++ pyflask/test-sample.py
@@ -2,9 +2,13 @@ import os
 import mysql.connector
 from mysql.connector import errorcode
 from pathlib import Path
-import init
+from init import *
 
-cursor.execute("SELECT * FROM Movies")
+query1 = '''
+SELECT title,releaseyear,runtimemin FROM Movies
+WHERE title LIKE "%Back to the Future%"
+'''
+cursor.execute(query1)
 for r in cursor.fetchall():
     print(r)
 
