b63d4b80968580360851350d8019a0fda4ca8454
Andy Bao <contact@andybao.me>
2021-06-13 22:34:13 -0400

    Vectorize movie_tables.py

--- transformation_scripts/movie_tables.py
+++ transformation_scripts/movie_tables.py
@@ -1,17 +1,24 @@
 import pandas as pd
 
+print('Reading original dataset...')
 title_basics = pd.read_csv('title.basics.tsv', sep='\t').drop(columns=['titleType', 'primaryTitle', 'isAdult', 'endYear'])
-genre_dict = {'tconst': [], 'genre': []}
 
-for index, row in title_basics.iterrows():
-    if type(row['genres']) is str:
-        for genre in row['genres'].split(','):
-            genre_dict['tconst'].append(row['tconst'])
-            genre_dict['genre'].append(genre)
+print('Executing transform 1/5...')
+movie_genre = title_basics.copy()
 
-movie_genre = pd.DataFrame(data=genre_dict)
+print('Executing transform 2/5...')
+movie_genre = movie_genre[movie_genre.genres.notnull() & (movie_genre.genres != '\\N')]
+
+print('Executing transform 3/5...')
+movie_genre['genre'] = movie_genre['genres'].str.split(',')
+
+print('Executing transform 4/5...')
+movie_genre = movie_genre[['tconst', 'genre']]
+movie_genre = movie_genre.explode('genre')
+
+print('Executing transform 5/5...')
 title_basics = title_basics.drop(columns=['genres']).rename(columns={'startYear': 'release_year', 'originalTitle': 'title'})
 
+print('Writing outputs...')
 title_basics.to_csv('movies.scsv', sep=';', index=False)
 movie_genre.to_csv('movie_genre.scsv', sep=';', index=False)
-
