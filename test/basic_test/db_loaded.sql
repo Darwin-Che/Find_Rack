febbce9a349197d9ed8efcc8a8e688b7dd096ef8
Zhaocheng Che <chezhaocheng@outlook.com>
2021-06-14 20:27:48 -0400

    test sample out complete

--- test/basic_test/db_loaded.sql
+++
@@ -1,7 +0,0 @@
-SELECT title FROM Movies ORDER BY titleid LIMIT 10;
-
-SELECT username FROM Users ORDER BY userid LIMIT 10;
-
-SELECT castname FROM Casts ORDER BY castid LIMIT 10;
-
-SELECT comment FROM Comments ORDER BY publishtime LIMIT 10;
